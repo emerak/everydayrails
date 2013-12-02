6d94872bd8c78c13649a7a460d2a007b1f714203
alejandra <alejandra@tangosource.com>
2013-12-02 09:58:56 -0600

    finished chapter 4

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,66 +3,73 @@ require 'spec_helper'
 describe Contact do
 
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:contact)).to be_valid
+    expect(create(:contact)).to be_valid
   end
 
   it 'is valid with a firstname, last name and email' do
-    contact = Factory.build(:contact)
-    expect(contact).to be_valid
+    expect(build(:contact)).to be_valid
   end
 
   it "is invalid without a firstname" do
-    contact = Factorygirl.build(:contact, firstname: nil)
-    expect(contact).to have(1).errors_on(:firstname)
+    expect(build(:contact, firstname: nil)).to  \
+      have(1).errors_on(:firstname)
   end
 
   it "is invalid without a lastname" do
-    contact = Factorygirl.build(:contact, lastname: nill)
-    expect(contact).to have(1).errors_on(:lastname)
+    expect(build(:contact, lastname: nil)).to \
+      have(1).errors_on(:lastname)
   end
 
   it 'is ivalidi without an email address' do
-    contact = Factorygirl.build(:contact, email: nil)
-    expect(contact).to have(1).errors_on(:email)
+    expect(build(:contact, email: nil)).to \
+      have(1).errors_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com')
-    contact = Contact.new(
-      firstname: 'Jane', lastname: 'Tester',
-      email: 'tester@example.com')
+    create(:contact,firstname: 'Joe', lastname: 'Tester',
+           email: 'tester@example.com')
+    contact = build(:contact,firstname: 'Jane', lastname: 'Tester',
+                    email: 'tester@example.com')
     expect(contact).to have(1).errors_on(:email)
   end
 
   it "returns a contact's full name as a string" do
-    contact = Factorygirl.buil(:contact, firstname: "Jane",
-                               lastame: "Doe" )
-
+    contact = build(:contact, firstname: "Jane",
+                    lastname: "Doe" )
     expect(contact.name).to eq 'Jane Doe'
   end
 
+  it "returns three phone numbers" do
+    expect(create(:contact).phones.count).to eq 3
+  end
+
   describe 'filter last name by letter' do
+
     before :each do
-     @smith = Contact.create(firstname: 'John', lastname: 'Smith',
-                            email: 'jsmith@example.com')
-     @jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
-                            email: 'tjones@example.com')
-     @johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
-                               email: 'jjohnson@example.com')
+     @smith = create(:contact, firstname: 'John', lastname: 'Smith',
+              email: 'jsmith@example.com')
+     @jones = create(:contact, firstname: 'Tim', lastname: 'Jones',
+              email: 'tjones@example.com')
+     @johnson = create(:contact, firstname: 'John', lastname: 'Johnson',
+                email: 'jjohnson@example.com')
     end
 
     context 'matching letters' do
+
       it "returns a sorted array of results that match" do
-        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+        expect(Contact.by_letter("J")).to  \
+          eq [@johnson, @jones]
       end
+
     end
 
     context 'non-matching letters' do
+
       it 'returns a sorted array of results that match' do
-        expect(Contact.by_letter("J")).to_not include @smith
+        expect(Contact.by_letter("J")).to_not \
+          include @smith
       end
+
     end
 
   end
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,24 +2,22 @@ require 'spec_helper'
 
 describe Phone do
   it 'does not allow duplicate phone numbers per contact' do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
-                            email: 'joetestes@example.com')
-    home_phone   = contact.phones.create(phone_type: 'home',
-                                      phone: '234345-5456-5464')
-    mobile_phone = contact.phones.build(phone_type: 'mobile',
-                                       phone:'234345-5456-5464')
+    contact = create(:contact)
+    home_phone = create(:home_phone,
+      contact: contact,
+      phone: '785-555-1234')
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
+      phone: '785-555-1234')
     expect(mobile_phone).to have(1).errors_on(:phone)
   end
 
   it "allows two contacts to share a phone number" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
-                             email: 'joetester@example.com')
-    contact.phones.create(phone_type: 'home',
-                          phone: '785-555-1234')
-    other_contact = Contact.create(firstname: 'John', lastname: 'Doe',
-                                   email: 'johndoe@tester.com')
-    other_phone = other_contact.phones.build(phone_type:'home',
-                                             phone: '785-555-1234')
-    expect(other_contact).to be_valid
+    create(:phone,
+      phone_type: 'home',
+      phone: '785-555-1234')
+    expect(build(:phone,
+      phone_type:'home',
+      phone: '785-555-1234')).to be_valid
   end
 end
